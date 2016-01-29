1a2de0cdb4faf2ab42fa127d58f8044dd288f00f
Oscar Nilsson <oscar@oncolour.se>
2016-01-28 17:28:46 -0700

    Turned into PHP

---
+++ includes/gallery.php
@@ -0,0 +1,41 @@
+<section id="portfolio">
+    <ul class="row">
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+        <li class="small-12 medium-4 large-4 columns">
+            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+        </li>
+    </ul>
+
+</section>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oncolour</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+
+
+    <section class="hero">
+        <nav role="navigation" class="row">
+            <div class="large-9 columns">
+                <a href="/"><img src="http://www.oncolour.se/wp-content/themes/oncolour-theme/img/logo-oncolour.png" alt="Logo home button"></a>
+            </div>
+            <div class="large-3 columns">
+                <ul class="row">
+                    <li class="large-4 columns"><a href="">Work</a></li>
+                    <li class="large-4 columns"><a href="">About</a></li>
+                    <li class="large-4 columns"><a href="">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="row intro">
+
+            <div class="small-centered medium-9 large-7 columns text-center">
+                <h1>Hi, I'm Oscar Nilsson</h1>
+                <p>A Swedish Web designer, currently living in Canada who’s passionate about interface design, usability and Wordpress.</p>
+                <a role="button" class="button large" href="#work">Check out my work</a>
+            </div>
+        </div>
+    </section>
+    <!-- Include gallery section -->
+    <?php include('includes/gallery.php'); ?>
+
+        <script src="bower_components/jquery/dist/jquery.js"></script>
+        <script src="bower_components/what-input/what-input.js"></script>
+        <script src="bower_components/foundation-sites/dist/foundation.js"></script>
+        <script src="js/app.js"></script>
+</body>
+
+</html>
