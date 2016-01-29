8fad124065aa93af919ac434ad9ec9fc3409abb0
Oscar Nilsson <oscar@oncolour.se>
2016-01-28 20:55:24 -0700

    removed hover text on one of the gallery images

--- includes/gallery.php
+++ includes/gallery.php
@@ -36,9 +36,7 @@
                     <figcaption>
                         <h2>Passionate <span>Julia</span></h2>
                         <div>
-                            <p>Julia dances in the deep dark</p>
-                            <p>She loves the smell of the ocean</p>
-                            <p>And dives into the morning light</p>
+
                         </div>
                         <a href="#">View more</a>
                     </figcaption>
