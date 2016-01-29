57d537931853c0530654edbd6ecdbfa9d6bd19b9
Oscar Nilsson <oscar@oncolour.se>
2016-01-28 17:32:19 -0700

    replace img placeholder

--- includes/gallery.php
+++ includes/gallery.php
@@ -1,40 +1,40 @@
 <section id="portfolio">
     <ul class="row">
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
         <li class="small-12 medium-4 large-4 columns">
-            <a href=""><img src="http://loremflickr.com/600/400/dog" alt=""></a>
+            <a href=""><img src="https://unsplash.it/600/400/?random" alt=""></a>
         </li>
     </ul>
 
