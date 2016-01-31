025cd78f6833d7ad6d6401444b93b13db8083f66
Oscar Nilsson <oscar@oncolour.se>
2016-01-30 18:51:27 -0700

    Added National dinosaur museum image

--- includes/gallery.php
+++ includes/gallery.php
@@ -4,13 +4,13 @@
         <ul class="row">
             <li class="small-12 medium-4 large-4 columns">
                 <figure class="effect-julia">
-                    <img src="https://unsplash.it/600/400/?random" alt="img21" />
+                    <img src="assets/images/the-national-dinosaur-museum-shot.jpg" alt="img21" />
                     <figcaption>
-                        <h2>Passionate <span>Julia</span></h2>
+                        <h2><span></span></h2>
                         <div>
-                            <p>Julia dances in the deep dark</p>
-                            <p>She loves the smell of the ocean</p>
-                            <p>And dives into the morning light</p>
+                            <p>The National Dinosaur Museum</p>
+                            <p>has the largest permanent display</p>
+                            <p>of dinosaurs and fossils in Australia.</p>
                         </div>
                         <a href="#">View more</a>
                     </figcaption>
