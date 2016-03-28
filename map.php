1af2c7730a958da2b9c5db8ae3fde0edf5f3bc6f
Oscar Nilsson <oscar@oncolour.se>
2016-03-27 21:39:16 -0600

    adding colours to Map and clickable countries

--- map.php
+++ map.php
@@ -1,7 +1,8 @@
 <?php require 'includes/page_header.php';?>
 
 
-    <div class="container"> <img src="assets/images/worldHigh.svg">
+    <div class="container">
+        <?php include 'assets/images/worldHigh.svg'?>
     </div>
 
     <?php require 'includes/page_footer.php';?>
