e5b9f582708c47095cb5e3d9bbd3cf905b4c36ba
Oscar Nilsson <oscar@oncolour.se>
2016-05-02 22:08:00 -0600

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,12 @@
 <footer id="page-footer">
-    
+     <div id="footer-profile-pic">
+         <img src="images/profile-pic-oscar-nilsson.jpg" alt="Oscar Nilsson Profile Picture">
+     </div>
+     <div id="footer-txt">
+         <h3>Hello, and thank you for taking the time to review my portfolio.</h3>
+         <p>I’m always looking for new challanges!</p>
+     </div>
+     <div class="start-new-project">
+         <a class="start-new-project-link" href="">Start a Project</a>
+     </div>
 </footer>
