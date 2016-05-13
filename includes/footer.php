60eceb97ef8c8d310a67a942600ed5a029d09535
Oscar Nilsson <oscar@oncolour.se>
2016-05-13 14:10:53 -0600

    adding x1 profile picture

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,8 @@
                     <source media="(max-width: 750px)" srcset="/images/profile-picture-oscar-nilsson2x.webp 2x, /images/profile-picture-oscar-nilsson3x.webp 3x" type="image/webp">
                     <source srcset="/images/profile-picture-oscar-nilsson2x.webp 1x, /images/profile-picture-oscar-nilsson3x.webp 2x" type="image/webp">
 
-                    <source media="(max-width: 750px)" srcset="/images/profile-picture-oscar-nilsson2x.jpg 2x, /images/profile-picture-oscar-nilsson3x.jpg 3x">
-                    <source srcset="/images/profile-picture-oscar-nilsson2x.jpg 1x, /images/profile-picture-oscar-nilsson3x.jpg 2x">
+                    <source media="(max-width: 750px)" srcset="/images/profile-picture-oscar-nilsson.jpg 1x, /images/profile-picture-oscar-nilsson2x.jpg 2x, /images/profile-picture-oscar-nilsson3x.jpg 3x">
+                    <source srcset="/images/profile-picture-oscar-nilsson.jpg 1x, /images/profile-picture-oscar-nilsson2x.jpg 1x, /images/profile-picture-oscar-nilsson3x.jpg 2x">
                     <img src="/images/profile-picture-oscar-nilsson2x.jpg" alt="Oscar Nilsson Profile Picture">
                 </picture>
         
