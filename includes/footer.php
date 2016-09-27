e315962eb8f96b239cc37236c90057b3d36b9fb3
Oscar Nilsson <oscar@oncolour.se>
2016-09-27 13:22:19 +0200

    adding social icons in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,42 @@
 <footer id="page-footer">
-     <div id="footer-profile-pic">
-        
-        <picture class="design-showcase flex100">
-                    <source media="(max-width: 750px)" srcset="/images/profile-picture-oscar-nilsson2x.webp 2x, /images/profile-picture-oscar-nilsson3x.webp 3x" type="image/webp">
-                    <source srcset="/images/profile-picture-oscar-nilsson2x.webp 1x, /images/profile-picture-oscar-nilsson3x.webp 2x" type="image/webp">
+    <div id="footer-profile-pic">
 
-                    <source media="(max-width: 750px)" srcset="/images/profile-picture-oscar-nilsson.jpg 1x, /images/profile-picture-oscar-nilsson2x.jpg 2x, /images/profile-picture-oscar-nilsson3x.jpg 3x">
-                    <source srcset="/images/profile-picture-oscar-nilsson.jpg 1x, /images/profile-picture-oscar-nilsson2x.jpg 1x, /images/profile-picture-oscar-nilsson3x.jpg 2x">
-                    <img src="/images/profile-picture-oscar-nilsson2x.jpg" alt="Oscar Nilsson Profile Picture">
-                </picture>
-        
-         
-     </div>
-     <div id="footer-txt">
-         <h3>Hello, and thank you for taking the time to review my portfolio.</h3>
-         <p>I’m always looking for new friends and challenges!</p>
-     </div>
-     <div class="start-new-project">
-         <a class="start-new-project-link" href="mailto:oscar@oncolour.se">Start a Project</a>
-     </div>
+        <picture class="design-showcase flex100">
+            <source media="(max-width: 750px)" srcset="/images/profile-picture-oscar-nilsson2x.webp 2x, /images/profile-picture-oscar-nilsson3x.webp 3x" type="image/webp">
+            <source srcset="/images/profile-picture-oscar-nilsson2x.webp 1x, /images/profile-picture-oscar-nilsson3x.webp 2x" type="image/webp">
+
+            <source media="(max-width: 750px)" srcset="/images/profile-picture-oscar-nilsson.jpg 1x, /images/profile-picture-oscar-nilsson2x.jpg 2x, /images/profile-picture-oscar-nilsson3x.jpg 3x">
+            <source srcset="/images/profile-picture-oscar-nilsson.jpg 1x, /images/profile-picture-oscar-nilsson2x.jpg 1x, /images/profile-picture-oscar-nilsson3x.jpg 2x">
+            <img src="/images/profile-picture-oscar-nilsson2x.jpg" alt="Oscar Nilsson Profile Picture">
+        </picture>
+
+
+    </div>
+    <div id="footer-txt">
+        <h3>Hello, and thank you for taking the time to review my portfolio.</h3>
+        <p>I’m always looking for new friends and challenges!</p>
+    </div>
+    <div class="start-new-project">
+        <a class="start-new-project-link" href="mailto:oscar@oncolour.se">Start a Project</a>
+        <ul class="social-icons-footer">
+            <li>
+                <a target="_blank" href="https://dribbble.com/stoggy"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a target="_blank" href="https://www.behance.net/stoggy"><i class="fa fa-behance" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a target="_blank" href="https://twitter.com/Stoggys"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a target="_blank" href="https://www.instagram.com/stoggy"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a target="_blank" href="https://www.linkedin.com/in/oscarbnilsson"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+            </li>
+            <li>
+                <a target="_blank" href="https://www.facebook.com/Oncolour/"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+            </li>
+        </ul>
+    </div>
 </footer>
